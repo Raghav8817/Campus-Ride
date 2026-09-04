d9b5c034166331721125d68f2a72d4959421a2a4
Raghav8817 <raghavsingh08724@gmail.com>
2026-09-05 00:56:16 +0530

    corrected 3 time

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,12 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/login': 'http://localhost:3000',
-      '/signup': 'http://localhost:3000',
-      '/logout': 'http://localhost:3000',
-      '/verify': 'http://localhost:3000',
-      '/user-data': 'http://localhost:3000'
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
+      }
     }
   }
 })
